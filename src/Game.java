87ffa0574e7c2571dc0c26c22ec404a37e25a55d
germinolegrand <germinolegrand@gmail.com>
2014-02-27 16:16:11 +0100

    Game

---
+++ src/Game.java
@@ -0,0 +1,16 @@
+/**
+ * Created by germinolegrand on 27/02/14.
+ */
+public class Game implements Drawable {
+
+    private Map currentLevel;
+
+    @Override
+    public void draw() {
+
+    }
+
+    public Game(Map currentLevel) {
+        this.currentLevel = currentLevel;
+    }
+}
