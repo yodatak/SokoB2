b148f70f693075b23c6885c5a8be394d09660960
eldiablosab <sabil.djerir@yahoo.fr>
2014-03-01 17:20:44 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	.idea/workspace.xml

---
+++ src/Score.java
@@ -0,0 +1,23 @@
+/**
+ * Created by yoyonice on 01/03/14.
+ * Score
+
+ scoring
+ etat
+
+ start()
+ stop()
+ save()
+ delete_scores()
+ */
+public class Score {
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    private int score;
+}
