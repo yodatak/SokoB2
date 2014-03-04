bcd21d1c2729cb6fecca026e1aeacbad34753f16
frédéric <lachenayefred@yahoo.fr>
2014-03-04 16:55:05 +0100

    Attributs Class Map : hauteur, largeur, vecteur implémentation méthodes get,set .

--- src/Map.java
+++ src/Map.java
@@ -1,12 +1,33 @@
+import java.util.Vector;
+//implémentation des vectors
+
 /**
  * Created by Lachenayefred on 27/02/14.
  */
-public class Map {
-    public static void main (String[] args){
-        System.out.println("Hello World");
-        // fred ?????? au boulot ?,,,,,,
 
+public class Map {
+
+    private int hauteur ;
+    private int largeur ;
+    Vector tab=new Vector();
+    /////////////////attributs de class map
+
+    public void setHauteur(int hauteur) { //////////// set hauteur
+        this.hauteur = hauteur;
     }
 
+    public void setLargeur(int largeur) { /////////// set largeur
+        this.largeur = largeur;
+    }
+
+    public int getHauteur() {   /////////////// get hauteur
+        return hauteur;
+    }
+
+    public int getLargeur() { //////////// get largeur
+        return largeur;
+    }
 }
+
+
 
