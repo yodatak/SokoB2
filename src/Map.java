8705ab730a63d11ce15f3b4cbe4f20d9a0564563
eldiablosab <sabil.djerir@yahoo.fr>
2014-03-05 21:17:10 +0100

    Game

--- src/Map.java
+++ src/Map.java
@@ -214,6 +214,11 @@ public class Map implements Drawable {
 
     public class ImpossibleMove extends Throwable {
     }
+
+    public void setResolveGame(boolean resolveGame) {
+        this.resolveGame = resolveGame;
+    }
+
     private boolean resolveGame = false;
 
 
